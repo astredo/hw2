a41712526e84f62df0c985bee55293ee598c4746
Astredo <seid.astrid@web.de>
2015-11-05 11:51:13 +0100

    output to file

--- logmap.cxx
+++ logmap.cxx
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -7,17 +8,24 @@ int main(){
 	double x;
 	int Nskip = 100; //Number of iterations to skip
 	int Nend  = 200; //Number of total iterations
-
-	for(double r=0; r <= 4; r += 0.001){
-	   x=x0;
-	   for(int i=1; i <= Nskip; i++)
+	double xfile[Nend - Nskip];
+	
+	ofstream out("output.dat");
+	  
+	for(double r = 0; r <= 4; r += 0.001){
+	   x = x0;
+	   for(int i=1; i <= Nskip; i++){
 		   x = r*x*(1-x);
-	   for(int i=Nskip+1; i <= Nend; i++){
-	   		   x = r*x*(1-x);
-	   		   cout << r << "\t" << x << endl;
-   	   }
+	   }
+	   xfile[0] = x;
+	   for(int i = 1; i <= Nend - Nskip; i++){
+	           xfile[i] = r*xfile[i-1]*(1-xfile[i-1]);
+      	   }
+	   for(int i = 1; i <= Nend - Nskip; i++){
+	           out << r << "\t" << xfile[i] << endl;
+	   }
 	}
 
-
+	out.close();
 	return 0;
 }
